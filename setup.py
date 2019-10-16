57a425046885cf1e64690c273cff707e4bfb82ea
whitequark <whitequark@whitequark.org>
2019-10-16 15:29:13 +0000

    setup: update scm_version().

--- setup.py
+++ setup.py
@@ -3,8 +3,15 @@ from setuptools import setup, find_packages
 
 def scm_version():
     def local_scheme(version):
-        return version.format_choice("+{node}", "+{node}.dirty")
-    return {"version_scheme": "guess-next-dev", "local_scheme": local_scheme}
+        if version.tag and not version.distance:
+            return version.format_with("")
+        else:
+            return version.format_choice("+{node}", "+{node}.dirty")
+    return {
+        "relative_to": __file__,
+        "version_scheme": "guess-next-dev",
+        "local_scheme": local_scheme
+    }
 
 
 setup(
