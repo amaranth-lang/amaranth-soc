1e1490ef85433493b9c43050eae8925ec85b2a53
Jean-François Nguyen <jf@jfng.fr>
2024-02-09 17:48:41 +0100

    csr.{reg,action}: update tests to follow RFC #27.

--- tests/test_csr_action.py
+++ tests/test_csr_action.py
@@ -20,12 +20,12 @@ class RTestCase(unittest.TestCase):
         def process():
             yield dut.r_data.eq(0xa)
             yield dut.port.r_stb.eq(1)
-            yield Settle()
+            yield Delay()
             self.assertEqual((yield dut.port.r_data), 0xa)
             self.assertEqual((yield dut.r_stb), 1)
 
         sim = Simulator(dut)
-        sim.add_process(process)
+        sim.add_testbench(process)
         with sim.write_vcd(vcd_file=open("test.vcd", "w")):
             sim.run()
 
@@ -43,12 +43,12 @@ class WTestCase(unittest.TestCase):
         def process():
             yield dut.port.w_data.eq(0xa)
             yield dut.port.w_stb.eq(1)
-            yield Settle()
+            yield Delay()
             self.assertEqual((yield dut.w_data), 0xa)
             self.assertEqual((yield dut.w_stb), 1)
 
         sim = Simulator(dut)
-        sim.add_process(process)
+        sim.add_testbench(process)
         with sim.write_vcd(vcd_file=open("test.vcd", "w")):
             sim.run()
 
@@ -75,14 +75,13 @@ class RWTestCase(unittest.TestCase):
             self.assertEqual((yield dut.data), 0x5)
             yield dut.port.w_stb .eq(1)
             yield dut.port.w_data.eq(0xa)
-            yield
-            yield Settle()
+            yield Tick()
             self.assertEqual((yield dut.port.r_data), 0xa)
             self.assertEqual((yield dut.data), 0xa)
 
         sim = Simulator(dut)
         sim.add_clock(1e-6)
-        sim.add_sync_process(process)
+        sim.add_testbench(process)
         with sim.write_vcd(vcd_file=open("test.vcd", "w")):
             sim.run()
 
@@ -111,21 +110,19 @@ class RW1CTestCase(unittest.TestCase):
             self.assertEqual((yield dut.data), 0xf)
             yield dut.port.w_stb .eq(1)
             yield dut.port.w_data.eq(0x5)
-            yield
-            yield Settle()
+            yield Tick()
             self.assertEqual((yield dut.port.r_data), 0xa)
             self.assertEqual((yield dut.data), 0xa)
 
             yield dut.port.w_data.eq(0x3)
             yield dut.set.eq(0x4)
-            yield
-            yield Settle()
+            yield Tick()
             self.assertEqual((yield dut.port.r_data), 0xc)
             self.assertEqual((yield dut.data), 0xc)
 
         sim = Simulator(dut)
         sim.add_clock(1e-6)
-        sim.add_sync_process(process)
+        sim.add_testbench(process)
         with sim.write_vcd(vcd_file=open("test.vcd", "w")):
             sim.run()
 
@@ -154,21 +151,19 @@ class RW1STestCase(unittest.TestCase):
             self.assertEqual((yield dut.data), 0x5)
             yield dut.port.w_stb .eq(1)
             yield dut.port.w_data.eq(0xa)
-            yield
-            yield Settle()
+            yield Tick()
             self.assertEqual((yield dut.port.r_data), 0xf)
             self.assertEqual((yield dut.data), 0xf)
 
             yield dut.port.w_data.eq(0x3)
             yield dut.clear.eq(0x7)
-            yield
-            yield Settle()
+            yield Tick()
             self.assertEqual((yield dut.port.r_data), 0xb)
             self.assertEqual((yield dut.data), 0xb)
 
         sim = Simulator(dut)
         sim.add_clock(1e-6)
-        sim.add_sync_process(process)
+        sim.add_testbench(process)
         with sim.write_vcd(vcd_file=open("test.vcd", "w")):
             sim.run()
 
